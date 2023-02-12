cc13956e07d31d43194312cdd0a53412134f6dd7
Suzzit <bajagainsuzzit@gmail.com>
2023-02-12 22:36:14 +0545

    minor change

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef, createRef } from 'react'
 function Home() {
   return (
     <>
+      This is the Homepage, Head to "Create" to generate Images
     </>
   )
 }
